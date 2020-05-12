8e1bed63f98b6ae53a617192d3d0948d6fe8452b
hersh-kat <h.kataria@warwick.ac.uk>
2020-05-12 17:25:49 +0100

    Converted code from Javascript to JSX

--- src/App.js
+++ src/App.js
@@ -3,24 +3,12 @@ import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", { id: "hello" }, [
-    React.createElement("h1", {}, "Adopt me!"),
-    React.createElement(Pet, {
-      name: "Honey",
-      type: "Dog",
-      colour: "Black",
-    }),
-    React.createElement(Pet, {
-      name: "Bob",
-      type: "Cat",
-      colour: "White",
-    }),
-    React.createElement(Pet, {
-      name: "Lol",
-      type: "Thing",
-      colour: "Rainbow",
-    }),
-  ]);
+  return (
+    <div>
+      <h1 id="something">Adopt me!</h1>
+      <Pet name="updoggg" colour="green" type="dog" />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 export default function Pet({ name, type, colour }){
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, type),
-    React.createElement("h2", {}, colour),
-  ]);
+ return (
+   <div>
+      <h1>{name}</h1>
+      <h2>{type}</h2>
+      <h2>{colour}</h2>
+   </div>
+ );
 }
