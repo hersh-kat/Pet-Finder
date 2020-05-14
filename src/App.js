bbed9906487ef64ddcb81b8d5a8ead99262e101e
hersh-kat <h.kataria@warwick.ac.uk>
2020-05-14 18:30:47 +0100

    Added new SearchParams component and custom hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import { SearchParams } from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="something">Adopt me!</h1>
-      <Pet name="updoggg" colour="green" type="dog" />
+      <SearchParams />
     </div>
   );
 };
