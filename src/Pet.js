7291d4cad3453d877f3376e55997aec7255e9ae6
hersh-kat <h.kataria@warwick.ac.uk>
2020-05-21 14:59:46 +0100

    Call API and display results

--- src/Pet.js
+++ src/Pet.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default function Pet({ name, type, colour }){
+export default function Pet({ name, breed, colour }){
  return (
    <div>
       <h1>{name}</h1>
-      <h2>{type}</h2>
+      <h2>{breed}</h2>
       <h2>{colour}</h2>
    </div>
  );
