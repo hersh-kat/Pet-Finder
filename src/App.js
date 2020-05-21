2a680ff71cd6123744136b9a48af1ba8a2bbdf8a
hersh-kat <h.kataria@warwick.ac.uk>
2020-05-21 22:24:42 +0100

    Fixed bug, did not use correct import syntax

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { SearchParams } from "./SearchParams";
 import { Router, Link } from "@reach/router";
-import Details from "./Details";
+import { Details } from "./Details";
 
 const App = () => {
   return (
